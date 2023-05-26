f8de35f00109d0402d23fc712287da1322aabc01
teto0613 <133244298+teto0613@users.noreply.github.com>
2023-05-26 22:56:27 +0000

    [finish] App1-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
   get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
